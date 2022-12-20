df0c9e270d36db22eacdd6a1ef33e3729b4048b3
Serhii P <sprymakov@mail.ua>
2022-12-20 16:06:56 +0200

    Added echoes of actions

---
+++ USCRIPT.bat
@@ -0,0 +1,13 @@
+@echo off
+echo Type the command:
+echo 1 - change HALID [admin]
+echo 2 - cwitch to OW6 or OW7 [admin]
+echo 3 - cwitch to LSSE2E or LGAUTO1 district [admin]
+echo 4 - copy the latest master build
+echo 5 - copy the Baseline build
+echo 6 - delete wallet folder
+echo 7 - delete DecisionSpace folder
+echo 8 - copy session(s) into DecisionSpace folder
+echo 9 - map drives
+
+@pause
